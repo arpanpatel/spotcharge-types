fdb511833fc801d891fbb05090d3ec815f760337
arpanpatel <patelarpan3832@gmail.com>
2023-09-21 11:33:03 +0530

    add tertiary key in color model

--- dist/models/color.model.d.ts
+++ dist/models/color.model.d.ts
@@ -2,5 +2,6 @@ export interface ColorModel {
     id: number;
     primary: string;
     secondary: string;
+    tertiary: string;
 }
 //# sourceMappingURL=color.model.d.ts.map
--- src/models/color.model.ts
+++ src/models/color.model.ts
@@ -2,4 +2,5 @@ export interface ColorModel {
     id: number,
     primary: string;
     secondary: string;
+    tertiary: string;
 }
