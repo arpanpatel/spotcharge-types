b807084146a984335f8684b10d254030d3e212fb
dips048 <dipeshpatel048@gmail.com>
2023-09-21 12:35:27 +0530

    removed chargerCapacityModel from ConnectorModel

--- src/models/charger-capacity.model.ts
+++
@@ -1,9 +0,0 @@
-import {CapacityUnit} from "../enum";
-import {ColorModel} from "./color.model";
-
-export interface ChargerCapacityModel {
-    id: string;
-    value: number;
-    unit: CapacityUnit;
-    color: ColorModel;
-}
