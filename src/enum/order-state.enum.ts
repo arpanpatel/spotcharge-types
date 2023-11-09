d632586590e141775bba85d5162f0a5f5a733afc
dips048 <dipeshpatel048@gmail.com>
2023-11-09 12:19:14 +0530

    order state enum added + order api model

---
+++ src/enum/order-state.enum.ts
@@ -0,0 +1,6 @@
+export enum OrderState {
+    Initiated = 'initiated',
+    Started = 'started',
+    InProgress = 'inProgress',
+    Completed = 'completed',
+}
--- src/models/api/order.api.model.ts
+++ src/models/api/order.api.model.ts
@@ -3,15 +3,17 @@ import {Timestamp} from "../timestamp";
 import {FleetModel} from "../fleet.model";
 import {FranchiseModel} from "../franchise.model";
 
-interface TransactionData {
-    [transactionId: string]: {
-        unitConsumed: number;
-        endTime: Date;
-        startTime: Date;
-        meterStop: number;
-        meterStart: number;
-        state: string;
-    };
+export interface ChargingTransaction {
+    unitConsumed: number;
+    endTime: Date;
+    startTime: Date;
+    meterStop: number;
+    meterStart: number;
+    state: string;
+}
+
+export interface ChargingTransactionRecords {
+    [transactionId: string]: ChargingTransaction;
 }
 
 export interface OrderApiModel {
@@ -27,7 +29,7 @@ export interface OrderApiModel {
     orderNo: number;
     transactionId: number;
     currentTransactionId: number;
-    transaction: TransactionData;
+    transaction: ChargingTransactionRecords;
     connectorNo: number;
     unitConsumed: number;
     meterStart: number;
@@ -36,11 +38,12 @@ export interface OrderApiModel {
     state: string;
     userId: string;
     unitRate: number;
+    orderType: string;
     createdBy: CreatedByModel;
     updatedBy: CreatedByModel;
+    deletedBy: CreatedByModel | null;
     createdAt: Timestamp;
     updatedAt: Timestamp;
     deletedAt: Timestamp | null;
     isDeleted: boolean;
-    orderType: string;
 }
