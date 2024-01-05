994398a23d571f6b1dd42be82e96b7b46ceff95d
arpanpatel <patelarpan3832@gmail.com>
2024-01-05 14:05:15 +0530

    update to new version 1.0.43

--- dist/models/user.model.d.ts
+++ dist/models/user.model.d.ts
@@ -32,7 +32,7 @@ export interface UserModel {
     providerData?: ProviderData[];
     fleet?: string[] | null;
     fleetUserId?: string;
-    allowedFleets?: string[];
+    allowedFleetIds?: string[];
 }
 export interface ProviderData {
     displayName: string;
