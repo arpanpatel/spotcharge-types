98fd509619bccebd43fb5d2f707409fb3e5af632
arpanpatel <patelarpan3832@gmail.com>
2024-02-16 19:47:23 +0530

    add payment info key in wallet transcation

--- dist/models/api/wallet-transaction.api.model.d.ts
+++ dist/models/api/wallet-transaction.api.model.d.ts
@@ -1,10 +1,12 @@
 import { CreatedByModel } from "../created-by.model";
 import { Timestamp } from "../timestamp";
 import { TransactionReason, TransactionType } from "../../enum";
+import { RazorpaySuccessResponse } from "../razorpay.model";
 export interface WalletTransactionApiModel {
     id: string;
     userId: string;
     amount: number;
+    paymentInfo: RazorpaySuccessResponse | null;
     type: TransactionType;
     reason: TransactionReason;
     createdBy: CreatedByModel;
--- src/models/api/wallet-transaction.api.model.ts
+++ src/models/api/wallet-transaction.api.model.ts
@@ -1,11 +1,13 @@
 import {CreatedByModel} from "../created-by.model";
 import {Timestamp} from "../timestamp";
 import {TransactionReason, TransactionType} from "../../enum";
+import {RazorpaySuccessResponse} from "../razorpay.model";
 
 export interface WalletTransactionApiModel {
   id: string;
   userId: string;
   amount: number;
+  paymentInfo: RazorpaySuccessResponse | null;
   type: TransactionType;
   reason: TransactionReason;
   createdBy: CreatedByModel;
