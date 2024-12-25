a4d9288bc5b1658dc66c67b10898ceff8e790cb8
Raj Lad <rklad04@gmail.com>
2024-12-25 11:17:36 +0530

    addres api model changes

--- src/models/api/address.api.model.ts
+++ src/models/api/address.api.model.ts
@@ -1,9 +1,14 @@
 export interface AddressApiModel {
   addressLine1: string;
-  state: string;
+  state: StateModel;
   street: string;
   country: string;
   postalCode: string;
   city: string;
   location: any
+}
+
+export interface StateModel {
+  code: string;
+  name: string;
 }
