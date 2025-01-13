f2dcc49e5c25598aa5714a00e50c59b265bc6e52
arpanpatel <patelarpan3832@gmail.com>
2025-01-13 13:07:33 +0530

    update types

--- dist/consts/in-out-type.const.d.ts
+++ dist/consts/in-out-type.const.d.ts
@@ -1,6 +1,6 @@
 export declare const InOutType: {
-    readonly VEHICLE: "vehicle";
-    readonly ASSET: "asset";
+    readonly vehicle: "vehicle";
+    readonly asset: "asset";
 };
 export type InOutKeys = keyof typeof InOutType;
 //# sourceMappingURL=in-out-type.const.d.ts.map
--- dist/consts/in-out-type.const.js
+++ dist/consts/in-out-type.const.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.InOutType = void 0;
 exports.InOutType = {
-    VEHICLE: 'vehicle',
-    ASSET: 'asset'
+    vehicle: 'vehicle',
+    asset: 'asset'
 };
 //# sourceMappingURL=in-out-type.const.js.map
