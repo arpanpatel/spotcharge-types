a152cc8a8f20634222a3cafc5934e7a534f25194
Raj Lad <rklad04@gmail.com>
2025-02-10 14:53:21 +0530

    permission const

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -3,3 +3,4 @@ export {InOutStatusType, InOutStatus} from './in-out-status.const';
 export {InOutType, InOutKeys} from './in-out-type.const';
 export {VehicleInspectionChecklist, VehicleInspectionChecklistType} from './vehicle-inspection-check-list.const';
 export {Notifications, NotificationType} from './notifications.const';
+export {permissionConfig, Permission, PermissionKeys} from './permission-config';
