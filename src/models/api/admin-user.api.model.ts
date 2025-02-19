3ab037985caa982ebe56793d77f142e11d46e2ac
Raj Lad <rklad04@gmail.com>
2025-02-19 12:02:51 +0530

    public order permission + display name in admin user api model

--- src/models/api/admin-user.api.model.ts
+++ src/models/api/admin-user.api.model.ts
@@ -7,6 +7,9 @@ export interface AdminUserApiModel {
     id: string;
     firstName: string;
     lastName: string;
+    displayName?: string;
+    photoURL: string | null;
+    color: string;
     userId: string;
     isOwner: boolean;
     email: string;
