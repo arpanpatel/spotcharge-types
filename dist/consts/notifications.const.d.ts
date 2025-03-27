f641e93086e0da26d28614c145acbab2900b8dff
arpanpatel <patelarpan3832@gmail.com>
2025-03-27 14:32:46 +0530

    update types

--- dist/consts/notifications.const.d.ts
+++ dist/consts/notifications.const.d.ts
@@ -1,5 +1,7 @@
 export declare const Notifications: {
     readonly scheduleBooking: "scheduleBooking";
+    readonly rescheduleBooking: "rescheduleBooking";
+    readonly completeCharging: "completeCharging";
     readonly cancelCharging: "cancelCharging";
     readonly refundCharging: "refundCharging";
     readonly stopCharging: "stopCharging";
--- dist/consts/notifications.const.js
+++ dist/consts/notifications.const.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Notifications = void 0;
 exports.Notifications = {
     scheduleBooking: "scheduleBooking",
+    rescheduleBooking: "rescheduleBooking",
+    completeCharging: "completeCharging",
     cancelCharging: "cancelCharging",
     refundCharging: "refundCharging",
     stopCharging: "stopCharging",
