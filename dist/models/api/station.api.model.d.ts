f0c857f503453e22d4ba511a7c6d26e0d571c743
arpanpatel <patelarpan3832@gmail.com>
2025-12-22 15:02:12 +0530

    remove franchise from station api model

--- dist/models/api/station.api.model.d.ts
+++ dist/models/api/station.api.model.d.ts
@@ -1,11 +1,9 @@
 import { AddressApiModel } from "./address.api.model";
 import { CreatedByModel } from "../created-by.model";
 import { Timestamp } from "../timestamp";
-import { FranchiseModel } from "../franchise.model";
 import { StationStatus } from "../../enum";
 export interface StationApiModel {
     id: string;
-    franchise: FranchiseModel;
     stationCode: string;
     name: string;
     address: AddressApiModel;
